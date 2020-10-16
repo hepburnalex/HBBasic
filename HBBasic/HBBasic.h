023629372cbfbe3324426d7a1016e12fbba0534e
hepburn <hepburn2019@163.com>
2020-10-16 14:42:05 +0800

    1

---
+++ HBBasic/HBBasic.h
@@ -0,0 +1,28 @@
+//
+//  HBBasic.h
+//  HBBasic
+//
+//  Created by Hepburn on 2019/7/1.
+//  Copyright © 2019 Hepburn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HBBasic.
+FOUNDATION_EXPORT double HBBasicVersionNumber;
+
+//! Project version string for HBBasic.
+FOUNDATION_EXPORT const unsigned char HBBasicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HBBasic/PublicHeader.h>
+
+
+
+#ifndef HBBasic_h
+#define HBBasic_h
+
+#import <HBBasic/AFHttpManager.h>
+#import <HBBasic/MBProgressHUD+MJ.h>
+
+#endif /* HBBasic_h */
+
